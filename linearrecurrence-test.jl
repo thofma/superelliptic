302a23e50b46a01b22eec45fb084e48b851f2234
Alex J. Best <alex.j.best@gmail.com>
2018-12-18 19:41:13 -0500

    random test

--- linearrecurrence-test.jl
+++ linearrecurrence-test.jl
@@ -2,13 +2,11 @@ using Nemo
 include("linearrecurrence.jl")
 
 
-Br= ResidueRing(ZZ,107)
-Ri,x = PolynomialRing(Br,'x')
-M = matrix(Ri,3,3,[0,0,2,105,0,0,0,105,0])
-L = [0];
-R = [103];
-s=Int64(floor(log(4,R[length(R)])));
-DDi= UpperCaseDD(Br(1),Br(2)^s,2^s)^(-1);
-println(parent(DDi))
-println(parent(M))
+Br= QadicField(107,2,2)
+Ri,t = PolynomialRing(Br,'t')
+M = matrix(Ri,5,5,[0, 0, 0, 0, 3*t, 131079598*t+520, 0, 0, 0, 3*t+131079497, 0, 131079598*t+520, 0, 0, 0, 0, 0, 131079598*t+520, 0, 0, 0, 0, 0, 131079598*t+520, 0])
+L =[0, 107, 214, 321]
+R = [101, 208, 315, 422]
+s=4
+DDi= Br(84664426)
 println(LinearRecurrence(M, L, R, DDi, s))
